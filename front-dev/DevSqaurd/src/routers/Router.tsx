e84a4a728d584bcc1211159bad7ecd8c5758e9ce
whgms159 <whgms159@naver.com>
2024-08-28 20:17:00 +0900

    feat : ProjectMainPage Layout

--- front-dev/DevSqaurd/src/routers/Router.tsx
+++ front-dev/DevSqaurd/src/routers/Router.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "../layouts/Layout";
 import TestPage from "../pages/TestPage";
+import ProjectMain from "../components/project/ProjectMain";
 
 export default function Router() {
   return (
     <Routes>
       <Route element={<Layout/>}>
         <Route path="/test" element={<TestPage/>} />  
+
+        <Route path="/project" element={<ProjectMain/>}/>
       </Route>
     </Routes>
   );
