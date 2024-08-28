6b620777e38bfab4fe377891884899dda6b069ab
DeepSoupp <mmaba@naver.com>
2024-08-28 19:13:05 +0900

    feat : sidebar init

---
+++ front-dev/DevSqaurd/src/components/mypage/SideBar.tsx
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <nav>
+      <ul>
+        <li><a href="https://www.naver.com">네이버</a></li>
+        <li><a href="https://www.kakao.com">카카오</a></li>
+        <li><a href="https://www.line.com">라인</a></li>
+        <li><a href="https://www.coupang.com">쿠팡</a></li>
+      </ul>
+    </nav>
+  </div>
+</body>
+</html>
+
