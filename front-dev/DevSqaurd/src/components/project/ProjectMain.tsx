64dfbe25d317cbc9ef15288416f4fed9cf718122
whgms159 <whgms159@naver.com>
2024-08-28 20:03:22 +0900

    feat : ProjectMainPage Layout

--- front-dev/DevSqaurd/src/components/project/ProjectMain.tsx
+++ front-dev/DevSqaurd/src/components/project/ProjectMain.tsx
@@ -1,28 +1,31 @@
-
-
+import style from './ProjectMain.module.css';
 
 export default function ProjectMain() {
   return (
-    <div>
-        <div>전체 프로젝트</div>
+    <div className={`${style.div1}`}>
+        <div className={`${style.test}`}>전체 프로젝트</div>
         <div>
-            <table>
-                <th>태그</th><th>태그</th><th>태그</th><th>태그</th><th>태그</th>
+            <table className={`${style.table}`}>
+                <th className={`${style.tag}`}><a href="#">태그</a></th>
+                <th className={`${style.tag}`}><a href="#">태그</a></th>
+                <th className={`${style.tag}`}><a href="#">태그</a></th>
+                <th className={`${style.tag}`}><a href="#">태그</a></th>
             </table>
         </div>
         <div>
-            <button>출시 서비스만 보기</button><button>프로젝트 생성</button>
+            <button>출시 서비스만 보기</button><button className={`${style.rigthbtn}`}>프로젝트 생성</button>
         </div>
         <div>
-            <ul>
-                <li>프로젝트</li>    
-                <li>프로젝트</li>   
-                <li>프로젝트</li>   
-                <li>프로젝트</li>   
-                <li>프로젝트</li>           
-                <li>프로젝트</li>   
-                <li>프로젝트</li>   
-                <li>프로젝트</li>   
+            <ul className={`${style.list}`}>
+                <li className={`${style.li}`}>프로젝트</li>    
+                <li className={`${style.li}`}>프로젝트</li>   
+                <li className={`${style.li}`}>프로젝트</li>   
+                <li className={`${style.li}`}>프로젝트</li>   
+                <br/> <br/> <br/> <br/> <br/> <br/> <br/> 
+                <li className={`${style.li}`}>프로젝트</li>   
+                <li className={`${style.li}`}>프로젝트</li>   
+                <li className={`${style.li}`}>프로젝트</li>   
+                <li className={`${style.li}`}>프로젝트</li>   
             </ul>   
         </div>  
        
