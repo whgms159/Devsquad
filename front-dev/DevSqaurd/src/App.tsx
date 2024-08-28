17ac7aa9c0b4ffdec3f5d44cd476e19d9c105fb5
whgms159 <whgms159@naver.com>
2024-08-28 19:07:03 +0900

    feat : project main page

--- front-dev/DevSqaurd/src/App.tsx
+++ front-dev/DevSqaurd/src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
+import ProjectMain from "./components/project/ProjectMain";
 
 function App() {
   return (
     <>
-      <h1>김태환 바보!</h1>
-      <h1>community</h1>
-      <h1>project</h1>
-      <h1>mypage</h1>
+      <ProjectMain></ProjectMain>
     </>
   );
 }
