db60e8d7c767b9e6775013182f327f04f067d796
whgms159 <whgms159@naver.com>
2024-08-28 19:06:24 +0900

    feat : project main page

---
+++ front-dev/DevSqaurd/src/components/project/ProjectMain.tsx
@@ -0,0 +1,31 @@
+
+
+
+export default function ProjectMain() {
+  return (
+    <div>
+        <div>전체 프로젝트</div>
+        <div>
+            <table>
+                <th>태그</th><th>태그</th><th>태그</th><th>태그</th><th>태그</th>
+            </table>
+        </div>
+        <div>
+            <button>출시 서비스만 보기</button><button>프로젝트 생성</button>
+        </div>
+        <div>
+            <ul>
+                <li>프로젝트</li>    
+                <li>프로젝트</li>   
+                <li>프로젝트</li>   
+                <li>프로젝트</li>   
+                <li>프로젝트</li>           
+                <li>프로젝트</li>   
+                <li>프로젝트</li>   
+                <li>프로젝트</li>   
+            </ul>   
+        </div>  
+       
+    </div>
+  )
+}
