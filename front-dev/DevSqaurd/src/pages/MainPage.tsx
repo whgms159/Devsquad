6e61ea7eb8f79fd9a0ee90d7dbf55d758959f1a7
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-08-29 20:37:11 +0900

    feat : landing(main)page

--- front-dev/DevSqaurd/src/pages/MainPage.tsx
+++ front-dev/DevSqaurd/src/pages/MainPage.tsx
@@ -1,8 +1,12 @@
+import Carousel from '../components/common/Carousel';
+import style from './styles/MainPage.module.css';
+
 const MainPage = () => {
     return (
-        <>
+        <div className={style.container}>
           <div>MainPage</div>
-        </>
+          <Carousel />
+        </div>
     );
 };
 
