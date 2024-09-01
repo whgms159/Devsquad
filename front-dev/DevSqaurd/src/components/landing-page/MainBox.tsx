4bd3a3b7035cf69f2469d54adf3fadaea55fa0a4
protofu <sungjae0512@gmail.com>
2024-09-01 19:29:18 +0900

    feat : landing page

---
+++ front-dev/DevSqaurd/src/components/landing-page/MainBox.tsx
@@ -0,0 +1,9 @@
+import mainImg from '../../assets/mainpage/mainpageImg.jpg';
+import style from './styles/MainBox.module.css';
+
+export default function MainBox() {
+  return (
+    <div className={style.container}>
+    </div>
+  );
+};
---
+++ front-dev/DevSqaurd/src/components/landing-page/RecruitBox.tsx
@@ -0,0 +1,14 @@
+import style from './styles/RecruitBox.module.css';
+
+export default function RecruitBox() {
+  return (
+    <div className={style.container}>
+      <div>
+        <p>here</p>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
+};
