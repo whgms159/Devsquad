d50290d05a5fbd5ed33e25bca35eded885af0d62
protofu <sungjae0512@gmail.com>
2024-09-02 23:03:40 +0900

    feat : small login box

--- front-dev/DevSqaurd/src/components/landing-page/MainBox.tsx
+++ front-dev/DevSqaurd/src/components/landing-page/MainBox.tsx
@@ -1,12 +1,33 @@
-import mainImg from '../../assets/mainpage/mainpageImg.jpg';
 import style from './styles/MainBox.module.css';
+import kakaoSymbol from '/assets/mainpage/kakaoSymbol.png';
 
 export default function MainBox() {
   return (
     <div className={style.container}>
-      <div className={style.loginBox}>
-        로그인 박스
+      <div className={style.loginContainer}>
+        <h4 className={style.loginTitle}>뭐라 써야할까요</h4>
+        <div className={style.loginInputBox}>
+          {/* 로그인 폼으로 이동시키기 */}
+          <form action="">
+            <div className={style.test}>
+              <div className={style.inputGroup}>
+                <input name='email' placeholder='Email' className={style.inputField}/>
+                <input type="password" name="password" placeholder='Password' className={style.inputField}/>
+              </div>
+              <div className={style.buttonGroup}>
+                <button className={style.loginButton}>Login</button>
+              </div>
+            </div>
+          </form>
+        </div>
+          {/* 회원가입 폼으로 이동시키기 */}
+          <button className={style.signupButton}>회원가입</button>
+          {/* 카카오 로그인 폼으로 이동시키기 */}
+          <div className={style.kakaoLoginContainer}>
+            <img src={kakaoSymbol} alt="카카오심볼" className={style.kakaoSymbolImg} />
+            <p className={style.kakaoLabel}>카카오 로그인</p>
+          </div>
       </div>
     </div>
   );
-};
+}
