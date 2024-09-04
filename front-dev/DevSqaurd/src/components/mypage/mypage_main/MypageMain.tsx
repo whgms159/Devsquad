91d01276a9dba2706a137c555932b3e9b21e4fba
DeepSoupp <mmaba@naver.com>
2024-09-05 00:38:07 +0900

    feat : mypage develop

---
+++ front-dev/DevSqaurd/src/components/mypage/mypage_main/MypageMain.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ProfileCard from "./ProfileCard";
+import style from "../styles/MypageMain.module.css"
+import ProjectSection from "./ProjectSection";
+
+const MypageMain: React.FC = () => {
+  return (
+    <div className={style.container}>
+      <ProfileCard 
+        nickname="김태환" 
+        language="java" 
+        streakCount={30} 
+        mannerTemperature={15} />
+        <ProjectSection />
+    </div>
+  );
+};
+
+export default MypageMain;
