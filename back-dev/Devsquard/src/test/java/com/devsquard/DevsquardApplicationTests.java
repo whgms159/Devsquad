219e389273dfd46b59304fe54a50e7c2d3a4f4a7
whgms159 <whgms159@naver.com>
2024-09-06 20:13:44 +0900

    init : backend

---
+++ back-dev/Devsquard/src/test/java/com/devsquard/DevsquardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsquard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSquardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
