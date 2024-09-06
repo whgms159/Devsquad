b1c962338e724af9d826aba7963289c04cebc6c2
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-09-06 18:52:13 +0900

    init : backend

---
+++ back-dev/DevSquard/src/main/java/com/example/demo/DevSquardApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSquardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSquardApplication.class, args);
+	}
+
+}
