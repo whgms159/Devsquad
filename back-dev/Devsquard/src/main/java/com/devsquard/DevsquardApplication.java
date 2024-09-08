aaf56f8260006ec6118589869c12b959c2fa5b68
protofu <sungjae0512@gmail.com>
2024-09-08 19:50:41 +0900

    feat : swagger

--- back-dev/Devsquard/src/main/java/com/devsquard/DevsquardApplication.java
+++ back-dev/Devsquard/src/main/java/com/devsquard/DevsquardApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
 @SpringBootApplication
 @EnableJpaAuditing
-public class DevSquardApplication {
+public class DevsquardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevSquardApplication.class, args);
+		SpringApplication.run(DevsquardApplication.class, args);
 	}
 
 }
