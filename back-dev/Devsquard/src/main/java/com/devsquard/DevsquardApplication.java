d4af5e1db8508745b7719bee65ac7f0c16a7bde3
protofu <sungjae0512@gmail.com>
2024-09-08 19:53:30 +0900

    fix : problem

--- back-dev/Devsquard/src/main/java/com/devsquard/DevsquardApplication.java
+++ back-dev/Devsquard/src/main/java/com/devsquard/DevsquardApplication.java
@@ -2,15 +2,12 @@ package com.devsquard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 
 @SpringBootApplication
-@EnableJpaAuditing
-public class DevsquardApplication {
+public class DevSquardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevsquardApplication.class, args);
+		SpringApplication.run(DevSquardApplication.class, args);
 	}
 
 }
