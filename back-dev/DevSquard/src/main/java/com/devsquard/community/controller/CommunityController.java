499787326dc864f97c56b3c83c3cb1fe2a8a5ebb
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-09-10 19:40:58 +0900

    feat : swagger success

--- back-dev/DevSquard/src/main/java/com/devsquard/community/controller/CommunityController.java
+++ back-dev/DevSquard/src/main/java/com/devsquard/community/controller/CommunityController.java
@@ -1,8 +1,16 @@
 package com.devsquard.community.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/community")
 public class CommunityController {
+	
+	@GetMapping("")
+	public String getPostList() {
+		return null;
+	}
 
 }
