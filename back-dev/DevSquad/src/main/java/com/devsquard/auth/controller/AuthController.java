2179c2aed07a94039de8dafba802812ce5b1e509
protofu <sungjae0512@gmail.com>
2024-09-11 23:44:31 +0900

    feat : jwt config & jwt-properties

--- back-dev/DevSquad/src/main/java/com/devsquard/auth/controller/AuthController.java
+++ back-dev/DevSquad/src/main/java/com/devsquard/auth/controller/AuthController.java
@@ -1,9 +1,9 @@
 package com.devsquard.auth.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +12,6 @@ import com.devsquard.auth.domain.SignUpResponse;
 import com.devsquard.auth.service.AuthService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
